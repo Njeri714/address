1546ca41d99395e2c6f1e7a7803db097d7b8e7f0
njeri waweru <wawerunjeri714@gmail.com>
2021-11-26 11:01:15 +0300

    added content in js

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function() {
+    $("#add-address").click(function() {
+      $("#new-addresses").append('<div class="new-address">' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-street">Street</label>' +
+                                     '<input type="text" class="form-control new-street">' +
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-city">City</label>' +
+                                     '<input type="text" class="form-control new-city">' +
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-county">County</label>' +
+                                     '<input type="text" class="form-control new-county">' +
+                                   '</div>' +
+                                 '</div>');
+    });
+    
 $(".contact").last().click(function() {
     $("#show-contact").show();
     $("#show-contact h2").text(newContact.firstName);
